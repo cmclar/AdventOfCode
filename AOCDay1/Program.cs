c366059089cdee3c69a16eeec968c5cf466a64ad
Craig McLaren <cmclarn@amazon.co.uk>
2022-12-06 14:38:37 +0000

    Initial Commit - Day 1

---
+++ AOCDay1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AOCDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            src.CalorieCalculator calculator = new src.CalorieCalculator();
+
+            calculator.ReadCalories("deps/input.txt");
+
+            int maxCalories = calculator.MaxCalories();
+
+            Console.WriteLine($"Max calories of any elf is: {maxCalories}");
+
+            int maxTopThree = calculator.MaxTopThree();
+
+            Console.WriteLine($"Total calories of the top 3 elfs is: {maxTopThree}");
+        }
+    }
+}
+
---
+++ AOCDay1/src/CalorieCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOCDay1.src
+{
+    public class CalorieCalculator
+    {
+        List<int[]> elfs;
+
+        public CalorieCalculator()
+        {
+            elfs = new List<int[]>();
+        }
+
+        public List<int[]> ReadCalories(string fileName)
+        {
+            var list = File.ReadAllLines(fileName);
+
+            List<int> elf = new List<int>();
+
+            foreach(string s in list)
+            {
+                int tempInt;
+                if (!string.IsNullOrEmpty(s) && int.TryParse(s, out tempInt))
+                {
+                    elf.Add(tempInt);
+                }
+                else
+                {
+                    if (elf.Any())
+                    {
+                        elfs.Add(elf.ToArray());
+                    }
+                    elf.Clear();
+                }
+            }
+
+            return elfs;
+        }
+
+        public int MaxCalories()
+        {
+            int maxCalories = elfs.Select(elf => elf.Sum()).Max();
+
+            return maxCalories;
+        }
+
+        public int MaxTopThree()
+        {
+            int maxTopThree = elfs.Select(elf => elf.Sum()).OrderByDescending(i => i).Take(3).Sum();
+
+            return maxTopThree;
+        }
+    }
+}
+
